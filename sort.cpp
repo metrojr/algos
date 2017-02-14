814aa2d4fa4902b9c8d70f8c52ee9b4b85b7718f
metrojr <metrojr@users.noreply.github.com>
2017-02-14 11:05:08 -0400

    Commit

--- sort.cpp
+++ sort.cpp
@@ -12,7 +12,7 @@ using namespace std;
 \param [in] data The data set that will be searched
 \returns location of key if found or -1 if not found
 */
-int linearSearch(auto data, auto key);//prototype
+void selectionSort(auto&data);//prototype
 
 
 int main()
